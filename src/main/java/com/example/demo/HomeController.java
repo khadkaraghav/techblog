1a921c5ca7e181fa08e026a2af33c3638bde6010
eyael <eyaelja1@jmail.com>
2019-02-22 14:50:36 -0500

    first commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-<<<<<<< HEAD
-public class HomeController {
-}
-=======
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,4 +14,3 @@ public class HomeController {
     }
 }
 
->>>>>>> 745af051baf4255d8db4839293b0541b17665ab1
